d843c3671a39c5ba66dda6c48e879e35a74f1e4a
Nicholas Klufas <nick@klufas.me>
2018-06-03 11:34:43 -0400

    Initial Commit
    Beep forever if there is an iphone in stock

---
+++ FindPhone/AppleResponse.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace FindPhone
+{
+   public partial class AppleResponse
+    {
+        [JsonProperty("body")]
+        public Body Body { get; set; }
+
+        [JsonProperty("head")]
+        public Head Head { get; set; }
+    }
+
+    public partial class Head
+    {
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+
+        [JsonProperty("status")]
+        public string Status { get; set; }
+    }
+
+    public partial class Body
+    {
+        [JsonProperty("availableStoreText")]
+        public string AvailableStoreText { get; set; }
+
+        [JsonProperty("availableStoresText")]
+        public string AvailableStoresText { get; set; }
+
+        [JsonProperty("noSimilarModelsText")]
+        public string NoSimilarModelsText { get; set; }
+
+        [JsonProperty("PickupMessage")]
+        public PickupMessage PickupMessage { get; set; }
+    }
+
+    public partial class PickupMessage
+    {
+        [JsonProperty("little")]
+        public bool Little { get; set; }
+
+        [JsonProperty("location")]
+        public string Location { get; set; }
+
+        [JsonProperty("notAvailableNearOneStore")]
+        public string NotAvailableNearOneStore { get; set; }
+
+        [JsonProperty("notAvailableNearby")]
+        public string NotAvailableNearby { get; set; }
+
+        [JsonProperty("overlayInitiatedFromWarmStart")]
+        public bool OverlayInitiatedFromWarmStart { get; set; }
+
+        [JsonProperty("productLocatorSpecialHours")]
+        public string ProductLocatorSpecialHours { get; set; }
+
+        [JsonProperty("productLocatorSpecialHoursView")]
+        public string ProductLocatorSpecialHoursView { get; set; }
+
+        [JsonProperty("recommendedProducts")]
+        public object[] RecommendedProducts { get; set; }
+
+        [JsonProperty("stores")]
+        public Store[] Stores { get; set; }
+
+        [JsonProperty("storesCount")]
+        public string StoresCount { get; set; }
+
+        [JsonProperty("viewMoreHoursLinkText")]
+        public string ViewMoreHoursLinkText { get; set; }
+    }
+
+    public partial class Store
+    {
+        [JsonProperty("address")]
+        public Address Address { get; set; }
+
+        [JsonProperty("city")]
+        public string City { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("directionsUrl")]
+        public string DirectionsUrl { get; set; }
+
+        [JsonProperty("hoursUrl")]
+        public string HoursUrl { get; set; }
+
+        [JsonProperty("makeReservationUrl")]
+        public string MakeReservationUrl { get; set; }
+
+        [JsonProperty("partsAvailability")]
+        public Data PartsAvailability { get; set; }
+
+        [JsonProperty("phoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [JsonProperty("reservationUrl")]
+        public string ReservationUrl { get; set; }
+
+        [JsonProperty("state")]
+        public string State { get; set; }
+
+        [JsonProperty("storeDistanceVoText")]
+        public string StoreDistanceVoText { get; set; }
+
+        [JsonProperty("storeDistanceWithUnit")]
+        public string StoreDistanceWithUnit { get; set; }
+
+        [JsonProperty("storeEmail")]
+        public string StoreEmail { get; set; }
+
+        [JsonProperty("storeHours")]
+        public StoreHours StoreHours { get; set; }
+
+        [JsonProperty("storeImageUrl")]
+        public string StoreImageUrl { get; set; }
+
+        [JsonProperty("storeListNumber")]
+        public long StoreListNumber { get; set; }
+
+        [JsonProperty("storeName")]
+        public string StoreName { get; set; }
+
+        [JsonProperty("storeNumber")]
+        public string StoreNumber { get; set; }
+
+        [JsonProperty("storedistance")]
+        public double Storedistance { get; set; }
+
+        [JsonProperty("storelatitude")]
+        public double Storelatitude { get; set; }
+
+        [JsonProperty("storelistnumber")]
+        public long Storelistnumber { get; set; }
+
+        [JsonProperty("storelongitude")]
+        public double Storelongitude { get; set; }
+    }
+
+    public partial class StoreHours
+    {
+        [JsonProperty("bopisPickupDays")]
+        public string BopisPickupDays { get; set; }
+
+        [JsonProperty("bopisPickupHours")]
+        public string BopisPickupHours { get; set; }
+
+        [JsonProperty("hours")]
+        public Hour[] Hours { get; set; }
+
+        [JsonProperty("storeHoursText")]
+        public string StoreHoursText { get; set; }
+    }
+
+    public partial class Hour
+    {
+        [JsonProperty("storeDays")]
+        public string StoreDays { get; set; }
+
+        [JsonProperty("storeTimings")]
+        public string StoreTimings { get; set; }
+    }
+
+    public partial class Data
+    {
+    }
+
+    public partial class Address
+    {
+        [JsonProperty("address2")]
+        public string Address2 { get; set; }
+
+        [JsonProperty("address3")]
+        public object Address3 { get; set; }
+
+        [JsonProperty("postalCode")]
+        public string PostalCode { get; set; }
+
+        [JsonProperty("address")]
+        public string PurpleAddress { get; set; }
+    }
+
+    public class Converter
+    {
+        public static readonly JsonSerializerSettings Settings = new JsonSerializerSettings
+        {
+            MetadataPropertyHandling = MetadataPropertyHandling.Ignore,
+            DateParseHandling = DateParseHandling.None,
+        };
+    }
+}
---
+++ FindPhone/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace FindPhone
+{
+    class Program
+    {
+        static Iphone[] arrayOfPhones = new Iphone[] {
+                new Iphone("https://www.apple.com/shop/pickup-message-recommendations?cppart=ATT/US&location=32137&product=MQAJ2LL/A", "Silver 256GB iPhone X"),
+                new Iphone("https://www.apple.com/shop/pickup-message-recommendations?cppart=ATT/US&location=32137&product=MQAM2LL/A", "Black 256GB iPhone X"),
+                new Iphone("https://www.apple.com/shop/pickup-message-recommendations?cppart=ATT/US&location=32137&product=MQAJ2LL/A", "Silver 64GB iPhone X"),
+                new Iphone("https://www.apple.com/shop/pickup-message-recommendations?cppart=ATT/US&location=32137&product=MQAK2LL/A", "Silver 256GB iPhone X")
+            };
+
+        static void Main(string[] args)
+        {
+            // Create a timer
+            Timer myTimer = new System.Timers.Timer();
+            // Tell the timer what to do when it elapses
+            myTimer.Elapsed += new ElapsedEventHandler(InvokeTheCalls);
+            // Set it to go off every 20 seconds
+            myTimer.Interval = 20000;
+            // And start it        
+            myTimer.Enabled = true;
+
+            // or automatically deserialize result
+            // return content type is sniffed but can be explicitly set via RestClient.AddHandler();
+            //RestResponse<AppleResponse> response2 = client.Execute<AppleResponse>(request);
+            Console.ReadLine();
+        }
+
+        public static void InvokeTheCalls(object source, ElapsedEventArgs e)
+        {
+            foreach (Iphone iphone in arrayOfPhones)
+            {
+                Console.WriteLine("Checking for: " + iphone.Model);
+                checkForPhone(iphone);
+            }
+        }
+
+        public static void checkForPhone(Iphone iphone)
+        {
+            var client = new RestClient(iphone.Url);
+            var request = new RestRequest(Method.GET);
+
+            IRestResponse response = client.Execute(request);
+            var content = response.Content; // raw content as string
+            var appleObject = JsonConvert.DeserializeObject<AppleResponse>(content);
+
+            if (appleObject.Body.PickupMessage.NotAvailableNearOneStore != "Not available today at the nearest store.")
+            {
+                // Beep infinitely.
+                for (int i = 0; i < int.MaxValue; i++)
+                {
+                    Console.Beep();
+                }
+            }
+
+            Console.WriteLine("None available");
+        }
+
+        public class Iphone
+        {
+            public Iphone(string url, string model)
+            {
+                this.Url = url;
+                this.Model = model;
+            }
+            public string Url { get; set; }
+            public string Model { get; set; }
+        }
+    }
+}
